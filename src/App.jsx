27525d727cfecf36975aefd3e4955b78bc1bcf54
Devloper-solaiman <devlopersolaiman@gmail.com>
2025-04-12 17:43:38 +0600

    hasto produt

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-7xl text-center mt-10 text-red-600 font-bold underline">
-      Hello world!
+      Hello Bistro Boss
     </h1>
   
      
